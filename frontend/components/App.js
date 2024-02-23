a15ed50e53ad1a30661e12c3bfc0125e9da263b8
Katy Kane <katytheartist@gmail.com>
2024-02-23 11:56:33 -0600

    displaying list from server

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,9 +1,41 @@
 import React from 'react'
+import axios from 'axios'
 
 const URL = 'http://localhost:9000/api/todos'
 
 export default class App extends React.Component {
+  state={
+    todos: [],
+  }
+
+  fetchAllTodos = () => {
+    axios.get(URL)
+    .then(res => {
+      
+      this.setState({...this.state, todos: res.data.data})
+    })
+    //.catch(this.setAxiosResponseError)
+    .catch(err =>{
+      debugger
+    })
+  }
+
+  componentDidMount(){
+    this.fetchAllTodos()
+  }
   render() {
-    return null
+    return (
+      <div>
+        <div></div>
+        <div id='todos'>
+          <h2>Todos:</h2>
+          {
+            this.state.todos.map(td =>{
+              return <div key={td.id}>{td.name}</div>
+            })
+          }
+        </div>
+      </div>
+    )
   }
 }
